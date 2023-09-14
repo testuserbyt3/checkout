a8d594b7d14b1060206c09cc02476e1180bc82cf
mohammadhamzailtaf <144703210+mohammadhamzailtaf@users.noreply.github.com>
2023-09-14 04:55:24 -0700

    Initial commit

---
+++ src/screens/Index/Index.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="component">
+          <p className="text-wrapper">Sale Is Live | Upto 98% Discount On Phones</p>
+        </div>
+        <div className="overlap">
+          <div className="text-wrapper-2">Delivery method</div>
+          <div className="overlap-group">
+            <div className="text-wrapper-3">Standard Delivery</div>
+            <p className="p">Estimated delivery time is 3-5 days</p>
+            <div className="rectangle-wrapper">
+              <div className="rectangle" />
+            </div>
+            <div className="text-wrapper-4">Free</div>
+          </div>
+          <img
+            className="line"
+            alt="Line"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502f37744b1e2ce0a958521/img/line-15.svg"
+          />
+        </div>
+        <div className="component-2">
+          <div className="text-wrapper-5">Payment methods</div>
+          <img
+            className="path"
+            alt="Path"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/path-6.svg"
+          />
+          <img
+            className="icon-metro-chevron"
+            alt="Icon metro chevron"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/icon-metro-chevron-thin-down.svg"
+          />
+          <div className="overlap-group-2">
+            <div className="rectangle-2" />
+            <div className="rectangle-3" />
+            <img
+              className="upi-icon-removebg"
+              alt="Upi icon removebg"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502f37744b1e2ce0a958521/img/upi-icon-removebg-preview.png"
+            />
+            <div className="text-wrapper-6">Pay With Upi Apps</div>
+          </div>
+          <div className="overlap-2">
+            <div className="overlap-3">
+              <img
+                className="element-visa-credit-card"
+                alt="Element visa credit card"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502f37744b1e2ce0a958521/img/363-visa-credit-card-logo-512-removebg-preview.png"
+              />
+              <img
+                className="mastercard-logo-PNG"
+                alt="Mastercard logo PNG"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502f37744b1e2ce0a958521/img/mastercard-logo-png-hd.png"
+              />
+            </div>
+            <div className="rectangle-4" />
+            <div className="text-wrapper-7">Credit / Debit Cards</div>
+          </div>
+          <div className="overlap-4">
+            <div className="rectangle-4" />
+            <img
+              className="img"
+              alt="Img"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502f37744b1e2ce0a958521/img/img-462181.png"
+            />
+            <div className="text-wrapper-7">Pay With Netbanking</div>
+          </div>
+          <div className="overlap-5">
+            <div className="rectangle-4" />
+            <img
+              className="element-paytm-logo"
+              alt="Element paytm logo"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502f37744b1e2ce0a958521/img/2560px-paytm-logo--standalone--svg.png"
+            />
+            <div className="text-wrapper-7">Pay With Paytm Wallet</div>
+          </div>
+        </div>
+        <div className="overlap-6">
+          <div className="text-wrapper-8">Total: ₹4,999</div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-9">Pay Now</div>
+          </div>
+        </div>
+        <div className="text-wrapper-10">Order Summary</div>
+        <img
+          className="line-2"
+          alt="Line"
+          src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-16.svg"
+        />
+        <img
+          className="line-3"
+          alt="Line"
+          src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-17.svg"
+        />
+        <img
+          className="line-4"
+          alt="Line"
+          src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-18.svg"
+        />
+        <div className="text-wrapper-11">Items</div>
+        <div className="text-wrapper-12">Subtotal</div>
+        <div className="text-wrapper-13">1 item</div>
+        <div className="text-wrapper-14">₹4,999</div>
+      </div>
+    </div>
+  );
+};
